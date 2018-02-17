cfbc980dee37611b850abdd11c2d03bd0cf85fdc
sandhya <konathalasandhya@gmail.com>
2018-02-17 17:18:27 +0100

    first git commit for selenium code

---
+++ src/test/java/testMaven/Sum.java
@@ -0,0 +1,33 @@
+package testMaven;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Sum {
+
+@Test
+public void sum1(){
+	System.out.println("the sum method");
+int a=10;
+int b=20;
+	Assert.assertEquals(a+b, 30);
+}
+
+
+@Test
+public void sub(){
+	System.out.println("the sub method");
+int a=10;
+int b=20;
+	Assert.assertEquals(b-a, 10);
+
+}
+@Test
+public void mul(){
+	System.out.println("the mul method");
+int a=10;
+int b=20;
+	Assert.assertEquals(a*b, 200);
+}
+}
